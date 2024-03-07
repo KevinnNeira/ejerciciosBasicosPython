cdd3bf959746415a7f8667ecf3635e2f094030a6
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-03-07 05:59:14 -0500

    parte decimal

--- parteDecimal.py
+++ parteDecimal.py
@@ -1,1 +1,4 @@
-num1 = float(input("Ingrese un numero: "))
+num1 = float(input("Ingrese un numero para hallar su parte decimal: "))
+a =  num1 - int(num1)
+a = abs(a)
+print(f"""La parte decimal de {num1} corresponde a: {a}""")
